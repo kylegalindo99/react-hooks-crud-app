76518f09768dc55e57108c3f627e8998148196ef
kgalindo666 <32023592+kgalindo666@users.noreply.github.com>
2020-04-22 17:09:17 -0700

    created main view / dummy data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
+import React, {useState} from "react";
+import UserTable from "./tables/UserTable";
 
-function App() {
+const App = () => {
+  const userData = [
+    { id: 1, name: "Tania", userName: "@taniatee22" },
+    { id: 2, name: "Craig", userName: "@craigjennings" },
+    { id: 3, name: "Jenny", userName: "@jenniferM" },
+    { id: 4, name: "Chad", userName: "@gothchad404" }
+  ];
+
+
+  // useState is a react hook!
+  // you can see we are passing the users as props to our UserTable component
+  // setUsers will be used to create a new user
+  const [users, setUsers] = useState(userData)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>CRUD App with Hooks</h1>
+      <div className="flex-row">
+        <div className="flex-large">
+          <h2>Add user</h2>
+        </div>
+        <div className="flex-large">
+          <h2>View users</h2>
+          <UserTable users={users}/>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
